746c622e2d7993bb2eb5b8e21032e6aaf1c7ef1c
Sami Labbaki <samilabbaki@MacBook-Pro-de-Sami.local>
2023-01-29 23:05:33 +0000

    add frontend logic

---
+++ rdao-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
